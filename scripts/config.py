5519c8d6d1603dabb55877c1d27ae61dac22c305
Djoystick <j.j.niccals2@gmail.com>
2025-11-28 09:52:38 +0300

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,9 +1,4 @@
 NEWS_SOURCES = [
-    {"name": "РБК Новости", "url": "https://rssexport.rbc.ru/rbcnews/news/20/full.rss", "category": "Новости"},
-    {"name": "Ведомости", "url": "https://www.vedomosti.ru/rss/news", "category": "Новости"},
-    {"name": "Фонтанка", "url": "https://fontanka.ru/fontanka.rss", "category": "Новости"},
-    {"name": "Лента", "url": "https://lenta.ru/rss", "category": "Новости"},
-    {"name": "Коммерсантъ", "url": "https://kommersant.ru/RSS/main.xml", "category": "Новости"},
     {"name": "РБК Мобайл", "url": "https://www.rbc.ru/mobileinternet/rss.xml", "category": "Технологии"},
     {"name": "Газета.ru Тех", "url": "https://www.gazeta.ru/export/rss/tech.xml", "category": "Технологии"},
     {"name": "Mail.ru Тех", "url": "https://news.mail.ru/rss/technology/", "category": "Технологии"},
@@ -12,21 +7,14 @@ NEWS_SOURCES = [
     {"name": "vc.ru", "url": "https://vc.ru/rss", "category": "Технологии"},
     {"name": "DTF", "url": "https://dtf.ru/rss/all", "category": "Игры"},
     {"name": "3DNews", "url": "https://3dnews.ru/news/rss/", "category": "Технологии"},
-    {"name": "NGS Новости", "url": "https://news.ngs.ru/rss/", "category": "Новости"},
     {"name": "Hi-Tech Mail.ru", "url": "https://hi-tech.mail.ru/rss/", "category": "Технологии"},
     {"name": "ixbt.com", "url": "https://ixbt.com/export/news.rss", "category": "Технологии"},
-    {"name": "Новая газета", "url": "https://novayagazeta.ru/rss/all", "category": "Новости"},
     {"name": "РИА Тех", "url": "https://ria.ru/export/rss2/tech/index.xml", "category": "Технологии"},
-    {"name": "Тасс", "url": "https://www.tass.ru/rss/v2.xml", "category": "Новости"},
     {"name": "Ведомости Технологии", "url": "https://www.vedomosti.ru/rss/rubric/technology", "category": "Технологии"},
-    {"name": "НГ", "url": "https://www.ng.ru/rss/", "category": "Новости"},
-    {"name": "Российская газета", "url": "https://rg.ru/xml/index.xml", "category": "Новости"},
     {"name": "Коммерсантъ Hi-Tech", "url": "https://www.kommersant.ru/RSS/section-hitech.xml", "category": "Технологии"},
     {"name": "3DNews Software", "url": "https://3dnews.ru/software/rss/", "category": "Технологии"},
     {"name": "ApiX-Drive", "url": "https://apix-drive.com/ru/rss/news.xml", "category": "Технологии"},
-    {"name": "NGS Технологии", "url": "https://ngs.ru/text/tech/rss/", "category": "Технологии"},
-    {"name": "E1 Новости", "url": "https://www.e1.ru/text/rss/", "category": "Новости"},
-    {"name": "74.ru Новости", "url": "https://74.ru/text/rss.xml", "category": "Новости"}
+    {"name": "NGS Технологии", "url": "https://ngs.ru/text/tech/rss/", "category": "Технологии"}
 ]
 
 EXCLUDE_KEYWORDS = [
